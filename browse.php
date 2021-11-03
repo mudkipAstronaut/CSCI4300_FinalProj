e7bd2a3da44586fcf5c3d4346fe5aaec867c67ee
Andres <mudkip461@gmail.com>
2021-11-03 19:54:57 -0400

    made the template for the browse page. need to add functionality

--- browse.php
+++ browse.php
@@ -12,21 +12,72 @@
       <li class="top_navlist"><a href="">Search</a></li>
       <li class="top_navlist"><a href="">Wishlist</a></li>
       <li class="top_navlist"><a href="">Login</a></li>
-      
-      <!--<li><span>Understanding UGA</span>
-	  <ul>
-	    <li><a href="/nutshell.html">UGA in a Nutshell</a></li>
-	    <li><a href="/points-pride.html">UGA Points of Pride</a></li>
-	    <li><a href="/history-uga.html">UGA’s History: A Brief look back</a></li>
-	  </ul>
-      </li>
-      -->
     </ul>
   </div>
 </nav>
 </div>
 </header>
+<div id="filters">
+  <h3>Filters</h3>
+  <form>
+    <div class="filter-block">
+      <h5>Checkboxes</h5>
+      <div>
+	<ul>
+	  <li><input id="checkbox1" type="checkbox"><label for="checkbox1">Option 1</label></li>
+	  <li><input id="checkbox2" type="checkbox"><label for="checkbox2">Option 2</label></li>
+	</ul>
+      </div>
+    </div>
+    
+    <div class="filter-block">
+      <h5>Select</h5>
+      <div>
+	<select>
+	  <option value="">Choose an option</option>
+	  <option value=".option1">Option1</option>
+	  <option value=".option2">Option2</option>
+	  <option value=".option3">Option3</option>
+	</select>
+      </div>
+    </div>
 
+    <div class="filter-block">
+      <h5>Radio</h5>
+      <div>
+	<ul>
+	  <li><input id="radio1" type="radio"><label for="radio1">Option 1</label></li>
+	  <li><input id="radio2" type="radio"><label for="radio2">Option 2</label></li>
+	</ul>
+      </div>
+    </div>
+  </form>
+</div>
 
+<div id="results">
+  <div class="pEntry">
+    <div>
+      <img src="place_imgs/london.jpg" alt="interesting">
+    </div>
+    <h2>City, Country</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Massa tincidunt dui ut ornare. Eu consequat ac felis donec et odio. Nunc non blandit massa enim nec dui. Blandit cursus risus at ultrices mi. Enim diam vulputate ut pharetra sit amet aliquam id. Vitae et leo duis ut diam quam. Mauris vitae ultricies leo integer malesuada nunc vel risus. Orci phasellus egestas tellus rutrum. Nisi quis eleifend quam adipiscing vitae proin sagittis. Sagittis orci a scelerisque purus semper eget duis.</p>
+  </div>
+
+  <div class="pEntry">
+    <div>
+      <img src="place_imgs/london.jpg" alt="interesting">
+    </div>
+    <h2>City, Country</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Massa tincidunt dui ut ornare. Eu consequat ac felis donec et odio. Nunc non blandit massa enim nec dui. Blandit cursus risus at ultrices mi. Enim diam vulputate ut pharetra sit amet aliquam id. Vitae et leo duis ut diam quam. Mauris vitae ultricies leo integer malesuada nunc vel risus. Orci phasellus egestas tellus rutrum. Nisi quis eleifend quam adipiscing vitae proin sagittis. Sagittis orci a scelerisque purus semper eget duis.</p>
+  </div>
+
+  <div class="pEntry">
+    <div>
+      <img src="place_imgs/london.jpg" alt="interesting">
+    </div>
+    <h2>City, Country</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Massa tincidunt dui ut ornare. Eu consequat ac felis donec et odio. Nunc non blandit massa enim nec dui. Blandit cursus risus at ultrices mi. Enim diam vulputate ut pharetra sit amet aliquam id. Vitae et leo duis ut diam quam. Mauris vitae ultricies leo integer malesuada nunc vel risus. Orci phasellus egestas tellus rutrum. Nisi quis eleifend quam adipiscing vitae proin sagittis. Sagittis orci a scelerisque purus semper eget duis.</p>
+  </div>
+</div>
 </body>
 </html>
