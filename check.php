9a82e3d8a9bc48716b3853597ee77c4e4590fc39
korth91900 <kyleorth919@gmail.com>
2021-11-11 16:54:18 -0500

    added login placeholder that uses cookies to save user login

---
+++ check.php
@@ -0,0 +1,20 @@
+<?php
+require('database.php');
+
+$name=$_POST['name'];
+$password=$_POST['password'];
+$check=$_POST['check'];
+
+$query="SELECT * FROM users WHERE username='$name' AND password='$password'";
+
+$data=$db->query($query);
+
+if($data->rowCount()>0) {
+	if($check=='1') {
+		setcookie("mycookie", TRUE, time()+60);
+	}
+	header('Location: home.php');
+} else {
+	header('Location: error.php');
+}
+?>
