9849960b16b746977f32dfc4c8270cac0b95f967
Andres <mudkip461@gmail.com>
2021-11-11 16:08:00 -0500

    added a general database.php file for all our pages to use. tested it in my page. it works

--- browse.php
+++ browse.php
@@ -1,3 +1,22 @@
+<?php
+include('database.php');
+
+//get a default value for an empty search
+//$category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
+//if($category_id == NULL || $category_id == FALSE){
+//$category_id = 1;
+//}
+//Get name for selected category
+//$queryCategory = 'SELECT * FROM categories WHERE categoryID=:category_id';
+//$statement1 = $db->prepare($queryCategory);
+//$statement1->bindValue(':category_id', $category_id);
+//$statement1->execute();
+//$category = $statement1->fetch();
+//$category_name = $category['categoryName'];
+//$statement1->closeCursor();
+
+?>
+
 <!DOCTYPE html> 
 <html>
 <link rel="stylesheet" href="style.css"/>
