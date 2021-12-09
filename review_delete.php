2ac9b21e7a35db152432e41177ac0c36538fb55a
Daniel Gyorfi <dg32081@uga.edu>
2021-12-09 14:13:58 -0500

    refactored score update code into separate file

--- review_delete.php
+++ review_delete.php
@@ -11,20 +11,5 @@ $db->query($delete);
 //update score 
 //=========================
 
-//code to get average and update place review score
-$query = "SELECT AVG(score) AS average FROM reviews WHERE placeID = ".$place_id;
-// $result = $db->prepare($query);
-$result = $db->query($query);
-// $statement->bindValue(':place_id', $place_id);
-$result->execute();
-$avgScore = $result->fetchAll()[0];
-$result->closeCursor();
-// echo $avgScore[0];
-// $avgScore = $result->fetch_assoc();//->fetchAll()[0];
-
-//update place review score
-$update = "UPDATE places SET reviewScore = ".$avgScore['average']." WHERE places.placeID = ".$place_id;
-$statement = $db->prepare($update);
-// $statement->bindValue(':place_id', $place_id);
-$statement->execute();
-?> 
+include('review_update_score.php');
+?>
--- review_edit.php
+++ review_edit.php
@@ -17,21 +17,5 @@ $statement->execute();
 //update score 
 //=========================
 
-//average all reviews for a place
-$query = "SELECT AVG(score) AS average FROM reviews WHERE placeID = ".$place_id;
-// $result = $db->prepare($query);
-$result = $db->query($query);
-// $statement->bindValue(':place_id', $place_id);
-$result->execute();
-$avgScore = $result->fetchAll()[0];
-$result->closeCursor();
-// echo $avgScore[0];
-// $avgScore = $result->fetch_assoc();//->fetchAll()[0];
-
-//update place review score
-$update = "UPDATE places SET reviewScore = ".$avgScore['average']." WHERE places.placeID = ".$place_id;
-$statement = $db->prepare($update);
-// $statement->bindValue(':place_id', $place_id);
-$statement->execute();
-$statement->closeCursor();
+include('review_update_score.php');
 ?> 
