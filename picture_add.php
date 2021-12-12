4166a1b8ac1f85111b42c73b55dfa36aa3efad25
Daniel Gyorfi <dg32081@uga.edu>
2021-12-12 17:14:12 -0500

    images can be added from place.php, might duplicate image on refresh

--- picture_add.php
+++ picture_add.php
@@ -8,6 +8,22 @@ if(!$noImage){
 	$targetPath = $targetPath . $fileName;
 	
 	move_uploaded_file($_FILES['fileUpload']['tmp_name'], $targetPath);		
+}	
+	
+function insertPic($noImage, $name, $db, $fileName, $sessionid) {
+	if (!$noImage) {
+		//gets placeID based on name, getting last placeID could introduce issues with simultaneous place addition
+		$getPlaceID = "SELECT placeID FROM places WHERE places.placeName = '$name'";
+		$statement = $db->prepare($getPlaceID);
+		$statement->execute();
+		$picPlaceID = $statement->fetchAll()[0]['placeID'];
+		$statement->closeCursor();
+		
+		$addPic = "INSERT INTO pictures (image,  placeID, userID)
+			VALUES ('$fileName', '$picPlaceID', '$sessionid')";
+		
+		$db->query($addPic);					
+	}
 }  
 
 insertPic($noImage, $name, $db, $fileName, $sessionid);
--- place.php
+++ place.php
@@ -2,9 +2,12 @@
 session_start();
 ?>
 <?php 
+//create session variables
 if(isset($_SESSION["loggedin"])) {
   $user_id = $_SESSION["uid"];
 }
+$loggedIn = isset($user_id);
+
 
 require('database.php');
 
@@ -25,6 +28,14 @@ try {
 		throw $ex;
 	}
 	echo $ex->getMessage();
+}
+
+//if this page was reached via the addPic button, it will run picture_add
+if($_SERVER["REQUEST_METHOD"] == "POST") {	
+	// $name needs to be set for picture_add 
+	$name = $results['placeName'];
+	$sessionid = $user_id;
+	include('picture_add.php');
 }
 
 //saving placeID for review.php
@@ -64,110 +75,109 @@ echo 'Added by [Deleted]';
 
 <div class="slideshow-container">
 
-<?php
-$imgQ = "SELECT image, userID FROM pictures WHERE placeID=" . $results['placeID'];
-$s2 = $db->prepare($imgQ);
-$s2->execute();
-$images = $s2->fetchAll();
-$s2->closeCursor();
+	<?php
+	$imgQ = "SELECT image, userID FROM pictures WHERE placeID=" . $results['placeID'];
+	$s2 = $db->prepare($imgQ);
+	$s2->execute();
+	$images = $s2->fetchAll();
+	$s2->closeCursor();
 
-$numImg = count($images);
-$counter=1;
-?>
+	$numImg = count($images);
+	$counter=1;
+	?>
 
-<!-- image container won't show if there are no images -->
-<?php if(!empty($images[0]['image'])) : ?>
+	<!-- image container won't show if there are no images -->
+	<?php if(!empty($images[0]['image'])) : ?>
 
-<?php
-foreach($images as $img): ?>
-<div class="mySlides fade">
+	<?php
+	foreach($images as $img): ?>
+	<div class="mySlides fade">
 
-     <div class="numbertext"><?php echo $counter.'/'.$numImg; ?></div>
-     <img src="place_imgs/<?php 
-     	  $imgPath = $img['image'];
-	  if(empty($imgPath)){
-	  $imgPath = 'default.jpg';
+		 <div class="numbertext"><?php echo $counter.'/'.$numImg; ?></div>
+		 <img src="place_imgs/<?php 
+			  $imgPath = $img['image'];
+		  if(empty($imgPath)){
+		  $imgPath = 'default.jpg';
+		  }
+		  echo $imgPath; 
+		  ?>" alt="interesting" style="width:100%;">
+		  
+		  <div class="text">
+		  <?php
+		  $userQ = 'SELECT username FROM users WHERE userID='. $img['userID'] .' LIMIT 1';
+		  $s2 = $db->prepare($userQ);
+		  $s2->execute();
+		  $username = $s2->fetchAll()[0];
+		  $s2->closeCursor();
+
+		  
+		  if(!empty($username)){
+		  echo 'Added by '. $username[0];
+		  }
+		  else{
+		  echo 'Added by [Deleted]';
+		  }
+		  ?>
+		  </div>
+	</div>
+
+	<?php
+	$counter++;
+	endforeach;
+	?>
+	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+	<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+	<script>
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  if (n > slides.length) {slideIndex = 1}    
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+		  slides[i].style.display = "none";  
 	  }
-	  echo $imgPath; 
-	  ?>" alt="interesting" style="width:100%;">
-	  
-      <div class="text">
-      <?php
-      $userQ = 'SELECT username FROM users WHERE userID='. $img['userID'] .' LIMIT 1';
-      $s2 = $db->prepare($userQ);
-      $s2->execute();
-      $username = $s2->fetchAll()[0];
-      $s2->closeCursor();
-
-      
-      if(!empty($username)){
-      echo 'Added by '. $username[0];
-      }
-      else{
-      echo 'Added by [Deleted]';
-      }
-      ?>
-      </div>
+	  slides[slideIndex-1].style.display = "block";  
+	}
+	</script>
+	<?php endif; ?>
 </div>
 
-<?php
-$counter++;
-endforeach;
-
-if(empty($images)){
-echo '
-<div class="mySlides fade">
-<div class="numbertext">1/1</div>
-<img src="place_imgs/default.jpg" alt="interesting" style="width:100%;">
-<div class="text">Added by Admin</div>
-</div>';
-}
-?>
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-<script>
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
-</script>
-<?php endif; ?>
-</div>
-<div style="display:inline">
-	<h4>Rating: <?php
+<!-- display review score -->
+<h4>Rating: <?php
 	if(!is_null($results['reviewScore'])){
 	echo $results['reviewScore'];
 	}
 	else{
 	echo 'No ratings yet';
 	}
-	?></h4>
-	
-</div>
+?></h4>
+
+<!-- add image form -->
+<?php if($loggedIn) : ?>
+	<label>Add an image
+		<form method="post" action="<?php echo "place.php?place=".$place;?>" enctype="multipart/form-data">
+			<input type="file" name="fileUpload" id="fileUpload" value="" class="loginInput" style="margin: 4px 0;"/>
+			<input type="submit" class="wishlistAddButton" value="Add Picture" id="submit" style="margin: 4px 0;"/>
+		</form>
+	</label>
+<?php endif; ?>
+
+<!-- description -->
 <p>
-<?php echo $results['description']; ?>
+	<?php echo $results['description']; ?>
 </p>
-<?php 
-$loggedIn = isset($user_id);
-?>
 
 <?php include('review.php'); ?>
 
